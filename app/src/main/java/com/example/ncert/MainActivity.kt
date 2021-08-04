5243b249f61c3297aae68e9618aad06108daef68
unknown <ga324261@gmail.com>
2021-08-04 21:30:47 +0530

    the Navigation from action bar is set

--- app/src/main/java/com/example/ncert/MainActivity.kt
+++ app/src/main/java/com/example/ncert/MainActivity.kt
@@ -2,16 +2,29 @@ package com.example.ncert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ncert.recycler.view.CustomAdapter
 import com.example.ncert.recycler.view.Data
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHost.navController
+        setupActionBarWithNavController(navController)
 
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
